da38c3bda9b88ecf6549f278eca23ffe312ae12c
JESWIN100 <josepjjeswin20@gmail.com>
2024-10-18 13:49:28 +0530

    vendum

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,4 @@
-import bcrypt  from 'bcrypt'
+import bcrypt  from 'bcryptjs'
 import { generateUserToken } from "../utils/generateToken.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { validateUserLogin, validateUserRegistration } from "../validation/userJoiValidation.js";
